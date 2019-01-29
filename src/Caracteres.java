b7f67f26aa6b7cb72a6a682accdbcf6fa677925c
joao <joao182cx@gmail.com>
2019-01-29 09:14:19 -0200

    Fontes aleatórias para os caracteres exibidos na janela.

--- src/Caracteres.java
+++ src/Caracteres.java
@@ -1,5 +1,7 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.GraphicsEnvironment;
 import java.security.SecureRandom;
 
 import javax.swing.JPanel;
@@ -7,17 +9,20 @@ import javax.swing.JPanel;
 public class Caracteres extends JPanel{
 	
 	final SecureRandom aleatorio = new SecureRandom();
-	
+	final String[] nomeFontes = {};
 	@Override
 	public void paintComponent(Graphics g) {
 		
 		super.paintComponent(g);
 		
-		
+		GraphicsEnvironment e = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		Font[] fonts = e.getAllFonts();
 		
 		for(int i = 0; i < aleatorio.nextInt(100000); i++) {
 			
 			g.setColor(new Color(aleatorio.nextInt(256), aleatorio.nextInt(256), aleatorio.nextInt(256)));
+			g.setFont(new Font(fonts[aleatorio.nextInt(fonts.length)].getName(), Font.PLAIN, 12));
+			
 			
 			char caractere = (char) aleatorio.nextInt(255);
 			g.drawString(Character.toString(caractere), aleatorio.nextInt(getWidth()), aleatorio.nextInt(getHeight()));
